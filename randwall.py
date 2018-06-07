654d94d9e915aec6355e64a92713d370298b0d18
RewoundVHS <nevelaughery@gmail.com>
2018-06-06 20:20:43 -0400

    Organized dirs, added random wallpaper/theme script

---
+++ randwall.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+import os
+import random
+import pwd
+
+home = pwd.getpwuid(os.getuid()).pw_dir
+wallDir = home + '/Pictures/Wallpapers/'
+
+randWall = wallDir + random.choice(os.listdir(wallDir))
+
+setWall = 'wal -i ' + randWall
+os.system(setWall)
